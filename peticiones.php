7028073d946c8ffe85a11ead611a6ea747a0f70d
Gerson Alvarado <gerson@yes.com.sv>
2020-04-10 14:00:47 -0600

    correcion de vinculo index en proyecto viejo

--- peticiones.php
+++ peticiones.php
@@ -7,12 +7,7 @@ $hash = "asdGsdf/678&/(fbrtyYH$%Efgsdg$";
 if ($_POST['login_peticion']) {
 	$r = $c->query("select * from usuarios where usuario = '".$_POST['usuario']."'");
 	var_dump($r);
-
-
 }
-
-
-
 
 
 
