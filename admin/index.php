83c19ac6b483544e284b988e2ffb38c84b1451f3
Arce <ronald.arce@sykes.com>
2020-04-19 12:24:05 -0600

    base con municipios

--- admin/index.php
+++
@@ -1,8 +0,0 @@
-<?php require 'control_sesion.php'; ?>
-
-<?php 
-if ($_SESSION['rol']!=1) {
-	header('location:../index.php');
-}
-
-var_dump($_SESSION); ?>
---
+++ logtest.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Jardineria DPWEB </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- JS here -->
+    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+</head>
+
+<body>
+
+    <!-- Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/logo.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader End -->
+
+	<!-- Header Start -->
+    <header>
+        <div class="header-area">
+            <div class="main-header ">
+                <div class="header-bottom  header-sticky">
+                    <div class="container-fluid">
+                        <div class="row align-items-center">
+                            <!-- Logo -->
+                            <div class="col-xl-1 col-lg-1 col-md-1 col-sm-3">
+                                <div class="logo">
+                                  <a href="index.php"><img src="assets/img/logo/logon.png" alt=""></a>
+                                </div>
+                            </div>
+                            <!-- Logo -->
+                            <div class="col-xl-6 col-lg-8 col-md-7 col-sm-5">
+                                <!-- Main-menu -->
+                                <div class="main-menu f-right d-none d-lg-block">
+                                    <nav>                                                
+                                        <ul id="navigation">                          
+                                            <li><a href="index.php">Inicio</a></li>
+                                            <li><a href="productos.php">Productos</a></li>
+                                            <li><a href="encuesta.php">Encuesta</a></li>
+                                            <li><a href="contact.php">Contacto</a></li>
+                                            <li><a href="#">Otros</a>
+                                                <ul class="submenu">
+                                                     <li><a href="oldproyect/historia.html">Historia del sitio</a> </li>
+                                                     <li><a href="oldproyect/plataformas.html">Plataformas Web</a> </li>
+                                                     <li><a href="oldproyect/maquetacion.html">Maquetación Web</a> </li>
+                                                     <li><a href="oldproyect/servidores.html">Servidores Web</a> </li>
+                                                     <li><a href="oldproyect/internet.html">Internet de las cosas</a> </li>
+                                                     <li><a href="oldproyect/info.html">Informacion del grupo</a> </li>
+                                                     <li><a href="oldproyect/glosario.html">Glosario</a> </li> 
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div> 
+                            <div class="col-xl-5 col-lg-3 col-md-3 col-sm-3 fix-card">
+                                <ul class="header-right f-right d-none d-lg-block d-flex justify-content-between">
+                                    <li class="d-none d-xl-block">
+                                        <div>
+                                        	<a href="index.php"><button type="button" class="btn btn-danger">Atras</button></a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- Mobile Menu -->
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header End -->
+
+	<section>
+		<div class="container">
+			<form>
+				<div class="row align-items-center">
+					<div style="text-align: center;">
+						<!-- Titulo -->
+						<h1>JARDINERIA DPWEB</h1><br>
+						<!-- Imagen -->
+						<br>
+						<img src="https://static.vecteezy.com/system/resources/previews/000/585/414/non_2x/vector-leaf-green-nature-logo-and-symbol-template.jpg" style="width: 250px;" alt="">
+						<br>
+						<!-- Leyenda -->
+						<h3> Inicio de sesión<br>Ingresa tus datos </h3>
+						<br>
+						<!-- Formulario -->
+						<form name="login" id="login" action="#" method="post" >
+
+						</form>
+
+						<div class="col-lg-6">
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+
+
+								<div class="col-md-3">
+								</div>
+								<div class="col-md-6 form-group">
+									<input required type="text" class="form-control valid" id="usuario" name="usuario" value="" placeholder="Usuario">
+								</div>
+								<div class="col-md-3">
+								</div>
+
+								<div class="col-md-12">
+								</div>
+
+								<div class="col-md-3">
+								</div>
+								<div class="col-md-6 form-group p_star">
+									<input required="" type="password" class="form-control" id="password" name="password" value="" placeholder="Password">
+								</div>
+								<div class="col-md-3">
+								</div>
+
+								<div class="col-md-12">
+									<label> </label>
+								</div>
+
+								<div class="col-md-12 form-group" align="center">
+									<button type="submit" class="btn btn-sucess" value="submit" class="btn-sm btn-primary">
+										Iniciar sesión
+									</button>
+									<br />
+									<br />
+									<a class="lost_pass" style="color: blue;" href="#">Olvide mi password</a>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+    
+
+<?php include 'pie.php'; ?>
