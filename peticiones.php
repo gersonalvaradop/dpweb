50855b7bae6fb65ee118ef2dd10750a7cd7d934c
Gerson Alvarado <gerson@yes.com.sv>
2020-04-22 15:47:08 -0600

    parte administrativa

--- peticiones.php
+++ peticiones.php
@@ -1,7 +1,13 @@
-<?php session_start(); ?>
-<?php 
-include 'mysql.php';
-$c = new Conexion();
+<?php session_start();
+
+function q($query)
+{
+	include 'conexion.php';
+	$c = mysqli_query($conexion, $query) or die(mysqli_error($conexion));
+	return $c;
+}
+
+
 $hash = "asdGsdf/678&/(fbrtyYH$%Efgsdg$";
 
 $_GET   = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
@@ -12,29 +18,36 @@ $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 if ($_POST['login_peticion']) {
 	$usuario = $_POST['usuario'];
 	$password = $_POST['password'];
-	$r = $c->query("select * from usuarios where usuario = '".$usuario."'");
-	if (empty($r) || $r==false) {
+	$r = q("select * from usuarios where usuario = '".$usuario."'");
+	
+	
+	
+
+	
+	if ($r->num_rows<=0) {
 		echo json_encode(array('exito'=>0,'mensaje'=>'No se encontro el usuario'));
 		die();
 	}
 
-	if ($r[0]['password']!=md5($hash.$password)) {
+	$data = $r->fetch_object();
+	if ($data->password!=md5($hash.$password)) {
 		echo json_encode(array('exito'=>0,'mensaje'=>'Password incorrecto'));
 		die();
 	}
 
 	$_SESSION['tiempo_actividad'] = time();
-	$_SESSION['nombre'] = $r[0]['nombre'];
-	$_SESSION['usuario'] = $r[0]['usuario'];
-	$_SESSION['correo'] = $r[0]['correo'];
-	$_SESSION['telefono'] = $r[0]['telefono'];
-	$_SESSION['rol'] = $r[0]['rol'];
-	$_SESSION['id'] = $r[0]['id'];
+	$_SESSION['nombre'] = $data->nombre;
+	$_SESSION['usuario'] = $data->usuario;
+	$_SESSION['correo'] = $data->correo;
+	$_SESSION['telefono'] = $data->telefono;
+	$_SESSION['rol'] = $data->rol;
+	$_SESSION['id'] = $data->id;
 
-	if ($r[0]['rol']==1) {
-		echo json_encode(array('exito'=>1,'mensaje'=>'Exito','nombre'=>$r[0]['nombre'], "redireccion"=>'admin.php'));
+
+	if ($data->rol==1) {
+		echo json_encode(array('exito'=>1,'mensaje'=>'Exito','nombre'=>$data->nombre, "redireccion"=>'admin/index.php'));
 	}else{
-		echo json_encode(array('exito'=>1,'mensaje'=>'Exito','nombre'=>$r[0]['nombre'], "redireccion"=>'users.php'));
+		echo json_encode(array('exito'=>1,'mensaje'=>'Exito','nombre'=>$data->nombre, "redireccion"=>'users.php'));
 	}
 
 
